f8453cbf31807d957742ad5127b5a0452a5517fe
yuchen <924761163@qq.com>
2019-07-24 21:03:38 +0800

    完成上传商品功能

--- src/controller/image.go
+++ src/controller/image.go
@@ -2,16 +2,16 @@ package controller
 
 import (
 	"github.com/kataras/iris"
-	"io"
-	"mime/multipart"
-	"os"
-	"path/filepath"
 	"../models"
 	"strconv"
+	"fmt"
+	"io"
+	"os"
 )
 
 func AddImage(ctx iris.Context) {
 	GoodsID := ctx.FormValue("goods_id")
+	index := ctx.FormValue("index")
 	maxSize := ctx.Application().ConfigurationReadOnly().GetPostMaxMemory()
 
 	err := ctx.Request().ParseMultipartForm(maxSize)
@@ -21,39 +21,15 @@ func AddImage(ctx iris.Context) {
 		return
 	}
 
-	form := ctx.Request().MultipartForm
+	file, info, _ := ctx.FormFile("files")
+	fmt.Println(file, info)
+	defer file.Close()
 
-	files := form.File["files[]"]
-	failures := 0
-	for _, file := range files {
-		_, err = saveUploadedFile(file, "../goods", GoodsID)
-		if err != nil {
-			failures++
-			ctx.Writef("failed to upload: %s\n", file.Filename)
-		}
-	}
-	ctx.Writef("%d files uploaded", len(files)-failures)
-}
+	out, err := os.OpenFile("../goods/"+ GoodsID + "-" + index + ".jpg", os.O_WRONLY|os.O_CREATE, 0666)
 
-
-func saveUploadedFile(fh *multipart.FileHeader, destDirectory string, goodsID string) (int64, error) {
-	src, err := fh.Open()
-	if err != nil {
-		return 0, err
-	}
-	defer src.Close()
-
-	out, err := os.OpenFile(filepath.Join(destDirectory, goodsID + "-" + fh.Filename),
-		os.O_WRONLY|os.O_CREATE, os.FileMode(0666))
-
-	if err != nil {
-		return 0, err
-	}
 	defer out.Close()
+	io.Copy(out, file)
 
-	id, _ := strconv.Atoi(goodsID)
-	models.Db.Create(&models.Image{GoodsID:id, Filename:goodsID + "-" + fh.Filename})
-
-	return io.Copy(out, src)
+	id, _ := strconv.Atoi(GoodsID)
+	models.Db.Create(&models.Image{GoodsID:id, Filename:GoodsID + "-" + index + ".jpg"})
 }
-
