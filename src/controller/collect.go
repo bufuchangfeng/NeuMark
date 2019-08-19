0804c9fafd74f99e74cc8044caf3c606556552a5
yuchen <924761163@qq.com>
2019-08-19 20:02:35 +0800

    hello

--- src/controller/collect.go
+++ src/controller/collect.go
@@ -13,6 +13,8 @@ func AddCollect(ctx iris.Context){
 	goods_id, _ := strconv.Atoi(ctx.FormValue("goods_id"))
 
 	collect := &models.Collect{UserID:user_id, GoodsID:goods_id}
+	models.Db.First(collect)
+	fmt.Println(collect)
 	if !(collect.ID > 0){
 		collect = &models.Collect{UserID:user_id, GoodsID:goods_id, CreatedAt:time.Now()}
 		models.Db.Create(collect)
