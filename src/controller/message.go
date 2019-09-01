027d6d35c2ef1387750bbdc88f73a20f398a3d7d
yuchen <924761163@qq.com>
2019-09-01 10:55:54 +0800

    hello

--- src/controller/message.go
+++ src/controller/message.go
@@ -3,15 +3,18 @@ package controller
 import (
 	"../models"
 	"github.com/kataras/iris"
-	// "strconv"
+	"strconv"
 )
 
 func GetMessages(ctx iris.Context){
-	// userid, _ := strconv.Atoi(ctx.FormValue("userid"))
+	userid, _ := strconv.Atoi(ctx.FormValue("userid"))
 	var messgaes []models.Message
-	models.Db.Where("").Find(&messgaes)
+	models.Db.Where("x_user_id = ?", userid).Find(&messgaes)
+
+	ctx.JSON(messgaes)
 }
 
 func DelMessage(ctx iris.Context){
-
+	id, _ := strconv.Atoi(ctx.FormValue("id"))
+	models.Db.Where("id", id).Delete(&models.Message{})
 }
